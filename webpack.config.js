1e85fde3e45f8290cc4245e33f73a27cb789161b
JhsK <qwe6293@nate.com>
2021-08-17 10:47:01 +0900

    초기 설정 및 구구단

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval', // hidden-source-map 프로덕션 모드 시 사용
+    resolve: {
+        extensions: ['.jsx', '.js', '.tsx', '.ts'],
+    },
+
+    entry: {
+        app: './client'
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+        }]
+    },
+    output: {
+        filename: '[name].js', //app.js
+        path: path.join(__dirname, 'dist'),
+    }
+}
